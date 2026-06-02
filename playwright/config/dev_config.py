4861f12a479666a7307d630ae491f760b4c97f06
Rohith Nandakumar <rohithkumar1990@gmail.com>
2026-06-02 18:14:12 +0530

    Playwright changes

--- playwright/config/dev_config.py
+++ playwright/config/dev_config.py
@@ -1,6 +1,7 @@
 from config.base_config import BaseConfig
 
 class DevConfig(BaseConfig):
-    BASE_URL = "https://dcsp.dev.walmart.com/US/"
+    BASE_URL = "https://practicetestautomation.com/practice/"
     ENV_NAME = "DEV"
+    API_KEY = "reqres_4c0974e8974c43208cf8c430937be05c"
     
